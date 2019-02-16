5033ab44eee4b80b81652b53e9da0d6a22caa2b9
petrero <petrero@gmail.com>
2019-02-16 16:56:45 +0200

    Run: ./bin/console doctrine:fixtures:load
    We have to change AccountFixtures.php to set various isActive values for accounts.

--- src/DataFixtures/AccountFixtures.php
+++ src/DataFixtures/AccountFixtures.php
@@ -6,10 +6,13 @@ use App\Entity\Account;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture {
+class AccountFixtures extends Fixture {
   public function load(ObjectManager $manager) {
     for ($i = 1; $i <= 20; ++$i) {
-      $manager->persist((new Account())->setUsername('jonsnow_'.$i));
+	    $account = new Account();
+	    $account->setUsername('jonsnow_'.$i);
+	    $account->setIsActive(0 == $i % 2 ? false : true);
+	    $manager->persist($account);
     }
 
     $manager->flush();
