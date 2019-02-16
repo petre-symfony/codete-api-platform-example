7cce55e828ab00a236aa1ecd23522d22eeb7538e
petrero <petrero@gmail.com>
2019-02-16 18:31:01 +0200

    Run: ./bin/console doctrine:migrations:dif
    Run: ./bin/console doctrine:migrations:migrate

---
+++ src/Migrations/Version20190216162826.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190216162826 extends AbstractMigration {
+  public function getDescription() : string {
+    return '';
+  }
+
+  public function up(Schema $schema) : void {
+    // this up() migration is auto-generated, please modify it to your needs
+    $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+    $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, symbol VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    $this->addSql('CREATE TABLE role_account (role_id INT NOT NULL, account_id INT NOT NULL, INDEX IDX_C1FBF35FD60322AC (role_id), INDEX IDX_C1FBF35F9B6B5FBA (account_id), PRIMARY KEY(role_id, account_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    $this->addSql('ALTER TABLE role_account ADD CONSTRAINT FK_C1FBF35FD60322AC FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE');
+    $this->addSql('ALTER TABLE role_account ADD CONSTRAINT FK_C1FBF35F9B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id) ON DELETE CASCADE');
+  }
+
+  public function down(Schema $schema) : void {
+    // this down() migration is auto-generated, please modify it to your needs
+    $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+    $this->addSql('ALTER TABLE role_account DROP FOREIGN KEY FK_C1FBF35FD60322AC');
+    $this->addSql('DROP TABLE role');
+    $this->addSql('DROP TABLE role_account');
+  }
+}
