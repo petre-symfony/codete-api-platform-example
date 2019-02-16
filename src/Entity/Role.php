402df4a2d746c35cb968aa94f587c72481273c66
petrero <petrero@gmail.com>
2019-02-16 23:06:47 +0200

    Role.php file has to be updated too, because while obtaining accounts we might not see any properties of nested roles

--- src/Entity/Role.php
+++ src/Entity/Role.php
@@ -6,12 +6,16 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiSubresource;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ApiResource()
+ * @ApiResource(attributes={
+ *   "normalization_context"={"groups"={"role.read"}},
+ *   "denormalization_context"={"groups"={"role.write"}}
+ * })
  * @ORM\Entity(repositoryClass="App\Repository\RoleRepository")
  * @ApiFilter(BooleanFilter::class, properties={"accounts.isActive"})
  */
@@ -20,22 +24,37 @@ class Role {
    * @ORM\Id()
    * @ORM\GeneratedValue()
    * @ORM\Column(type="integer")
+   * @Groups({
+   *  "role.read"
+   * })
    */
   private $id;
 
   /**
    * @ORM\ManyToMany(targetEntity="App\Entity\Account", inversedBy="roles")
    * @ApiSubresource(maxDepth=1)
+   *
+   * @Groups({
+   *  "role.read"
+   * })
    */
   private $accounts;
 
   /**
    * @ORM\Column(type="string", length=255)
+   * @Groups({
+   *  "role.read",
+   *  "role.write"
+   * })
    */
   private $symbol;
 
   /**
    * @ORM\Column(type="text", nullable=true)
+   * @Groups({
+   *  "role.read",
+   *  "role.write"
+   * })
    */
   private $description;
 
